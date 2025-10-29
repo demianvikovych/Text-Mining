9f34c2f9f1b5126321a4bef7026d672e63c86cf4
Demian Vikovych <demvikovych@gmail.comgit add.git config user.email demvikovych@gmail.com>
2025-10-29 18:15:58 +0100

    29-10-2025 DV

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\DV\Text Mining\.venv
+set VIRTUAL_ENV=C:\Users\s-A013-11\Desktop\Text-Mining\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
